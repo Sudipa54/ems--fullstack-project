b0b3a2d87657eeb72719c2389904300d4c946aca
Sudipa54 <sudipa6054@gmail.com>
2024-10-04 21:59:17 -0400

    Implement react-router-dom in main component

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,48 +1,48 @@
-import Login from "./Components/login";
-import AdminLogin from "./Components/adminlogin";
-import Dashboard from "./Components/dashboard";
-import Employee from "./Components/employee";
-import Category from "./Components/category";
-import Profile from "./Components/profile";
-import EmployeeDetail from "./Components/EmployeeDetail";
-import EmployeeEdit from "./Components/employeeEdit";
-import AddEmployee from "./Components/addEmployee";
-import AddCategory from "./Components/addCategory";
-import Home from "./Components/home";
-import PrivateRoute from "./Components/PrivateRoute";
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />}></Route>
-        <Route path="/adminlogin" element={<AdminLogin />}></Route>
-        <Route path="/employee_detail" element={<EmployeeDetail />}></Route>
-        <Route path="/home" element={<Home />}></Route>
+// import Login from "./Components/login";
+// import AdminLogin from "./Components/adminlogin";
+// import Dashboard from "./Components/navigation";
+// import Employee from "./Components/employee";
+// import Category from "./Components/category";
+// import Profile from "./Components/profile";
+// import EmployeeDetail from "./Components/EmployeeDetail";
+// import EmployeeEdit from "./Components/employeeEdit";
+// import AddEmployee from "./Components/addEmployee";
+// import AddCategory from "./Components/addCategory";
+// import Home from "./Components/home";
+// import PrivateRoute from "./Components/PrivateRoute";
+// import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Login />}></Route>
+//         <Route path="/adminlogin" element={<AdminLogin />}></Route>
+//         <Route path="/employee_detail" element={<EmployeeDetail />}></Route>
+//         <Route path="/home" element={<Home />}></Route>
 
-        <Route path="/dashboard" element={<Dashboard />}>
-          <Route path="" element={<Home />}></Route>
-          {/* <Route path="/dashboard" element={<Dashboard />}></Route> */}
-          <Route index element={<Home />} />
-          <Route path="employee" element={<Employee />} />
-          <Route path="category" element={<Category />}></Route>
-          <Route path="profile" element={<Profile />}></Route>
-          <Route
-            path="/dashboard/employee_edit"
-            element={<EmployeeEdit />}
-          ></Route>
-          <Route
-            path="/dashboard/add_employee"
-            element={<AddEmployee />}
-          ></Route>
-          <Route
-            path="/dashboard/add_category"
-            element={<AddCategory />}
-          ></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+//         <Route path="/dashboard" element={<Dashboard />}>
+//           <Route path="" element={<Home />}></Route>
+//           {/* <Route path="/dashboard" element={<Dashboard />}></Route> */}
+//           <Route index element={<Home />} />
+//           <Route path="employee" element={<Employee />} />
+//           <Route path="category" element={<Category />}></Route>
+//           <Route path="profile" element={<Profile />}></Route>
+//           <Route
+//             path="/dashboard/employee_edit"
+//             element={<EmployeeEdit />}
+//           ></Route>
+//           <Route
+//             path="/dashboard/add_employee"
+//             element={<AddEmployee />}
+//           ></Route>
+//           <Route
+//             path="/dashboard/add_category"
+//             element={<AddCategory />}
+//           ></Route>
+//         </Route>
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
 
-export default App;
+// export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,24 +1,24 @@
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ReactDOM, { createRoot } from "react-dom/client";
 import React from "react";
-import App from "./app";
+import Login from "./Components/login";
+import AdminLogin from "./Components/adminlogin";
+
 import "./global.css";
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App />,
-//   },
-// ]);
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     <RouterProvider router={router} />
-//   </StrictMode>
-// );
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/adminlogin",
+    element: <AdminLogin />,
+  },
+]);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
 );
